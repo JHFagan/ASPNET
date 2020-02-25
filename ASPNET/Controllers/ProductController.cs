2141ca6349c9448e7fe45531c5fb426f83d3c7b8
Jack Fagan <jhfagan579@gmail.com>
2020-02-25 09:57:21 -0600

    added update and create item functionality

---
+++ ASPNET/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNET.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+        public IActionResult UpdateProduct(int id)
+        {
+            Product product = repo.GetProduct(id);
+            repo.UpdateProduct(product);
+            if (product == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(product);
+        }
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+            return View(prod);
+        }
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
